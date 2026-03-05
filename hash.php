ba9697cf9a4de4450b6683551b9bc5d238325714
Lailin Nurasiyah <lailinnurasiyah@gmail.com>
2026-03-05 16:12:11 +0700

    add change

---
+++ hash.php
@@ -0,0 +1,8 @@
+<?php
+echo password_hash("rani123", PASSWORD_DEFAULT)."<br>";
+echo password_hash("victor123", PASSWORD_DEFAULT)."<br>";
+echo password_hash("elis123",PASSWORD_DEFAULT)."<br>";
+echo password_hash("wesley123",PASSWORD_DEFAULT)."<br>";
+echo password_hash("alivia123",PASSWORD_DEFAULT)."<br>";
+echo password_hash("dewi123",PASSWORD_DEFAULT)."<br>";
+echo password_hash("atik123",PASSWORD_DEFAULT)."<br>";
